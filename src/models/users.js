6f3f7fdc2c24216c63bb91cf506e32e2f02a2312
James Denton <sgtbelly@gmail.com>
2018-10-23 21:42:52 -0700

    added users.js

--- src/models/users.js
+++ src/models/users.js
@@ -1,3 +1,36 @@
 'use strict';
 
 import storage from '../lib/storage/memory.js';
+// import storage from 'filestorage';
+// import storage from 'mongostuff';
+
+class Users {
+
+  static findOne(id) {
+    let query = { _id:id };
+    return this.find(query);
+  }
+
+  static find(query) {
+    return storage.find(query);
+  }
+
+  static save(data) {
+    return storage.save(data);
+  }
+
+  static delete(id) {
+    return storage.delete(id);
+  }
+
+  static put(id, data) {
+    return storage.delete(id);
+  }
+
+  static patch(id, data) {
+    data._id = id;
+    return storage.save(data);
+  }
+}
+
+export default Users;
